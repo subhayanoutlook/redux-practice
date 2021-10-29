a6a451d8f6144f2c026d698def0fc9eb54b93f58
Subhayan Sarkar <50056241+subhayanoutlook@users.noreply.github.com>
2021-10-29 20:25:30 +0530

    remove test variable

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import RightDownComponent from "./components/RightDownComponent";
 import {Provider} from "react-redux";
 import store from "./redux/store";
 
-const test="hello";
 
 class App extends Component {
     render() {
